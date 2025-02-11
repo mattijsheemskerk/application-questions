c27e15da3933ff3eb93ce667b9eda4b09e4ae3c2
Mattijs Heemskerk <mattijsheemskerk@gmail.com>
2025-02-11 16:03:57 +0100

    Q1 and Q2 first version

---
+++ nfi/src/Q2/Q2Heptadecimaal.java
@@ -0,0 +1,41 @@
+package Q2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2Heptadecimaal {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+        	String line;
+        	while ((line = bufferedReader.readLine()) != null) {
+            	char[] tokens = line.toCharArray();
+            	int maxbaseindex = tokens.length - 1;
+        		int numberConverted = 0;
+
+        		if (line.equals("x")) {
+	        		bufferedReader.close();
+	        		break;
+	        	}
+		        for (int i = 0; i <= maxbaseindex; i++) {
+	        		int numberPerPosition;
+		        	if (!Character.isDigit(tokens[i])) {
+		        		numberPerPosition = tokens[i] - 'a' + 10;
+		        	}
+		        	else numberPerPosition = Character.getNumericValue(tokens[i]);
+		        	
+		        	numberConverted += (numberPerPosition * 17^(maxbaseindex  - i));
+		        	System.out.println(numberConverted + " = numberConverted");
+		        	System.out.println(numberPerPosition + " = numberPerPosition on position i = " + i + "," + tokens.length);
+	        	}
+		        System.out.println(numberConverted + " = numberConverted");
+        	}
+    		bufferedReader.close();
+        }
+        catch(Exception e) {
+        	bufferedReader.close();
+        }
+    }
+}
