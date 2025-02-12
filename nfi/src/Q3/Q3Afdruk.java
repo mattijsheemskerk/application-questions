6814f5f3919d2dc9540f5aa459fcc93447dae29c
Mattijs Heemskerk <mattijsheemskerk@gmail.com>
2025-02-12 15:19:19 +0100

    First working version of Q3.

---
+++ nfi/src/Q3/Q3Afdruk.java
@@ -0,0 +1,43 @@
+package Q3;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q3Afdruk {
+
+	public static void main(String[] args) throws IOException {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        	String line;
+        	int lineNumber = 1;
+        	int numberOfColumns = 0;
+        	while ((line = bufferedReader.readLine()) != null) {
+        		if (line.equals("0")) {
+	        		bufferedReader.close();
+	        		break;
+	        	}
+        		if (lineNumber % 2 != 0) {
+        			numberOfColumns = Integer.valueOf(line);
+        		}
+        		else {
+        			char[] tokens = line.toCharArray();
+        			char[] result = new char[tokens.length];
+        			int numberOfRows = tokens.length / numberOfColumns;
+
+        			for (int i = 0; i < tokens.length; i++) {
+        				int indexPerColumn = Math.abs((i) / numberOfColumns);
+        				int rowNumber = (i % numberOfColumns);
+        				//System.out.println(i + ": columnNumber=" + columnNumber + ", rowNumber=" + rowNumber + ", index=" + (columnNumber + (rowNumber * numberOfRows)));
+        				result[indexPerColumn + (rowNumber * numberOfRows)] = tokens[i];
+        			}
+    		        System.out.println(String.valueOf(result));
+        		}
+        		lineNumber++;
+        	}
+    		bufferedReader.close();
+        }
+        catch (Exception e) {
+        	System.out.println("Input error");
+        }
+    }
+}
